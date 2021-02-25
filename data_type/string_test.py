3b3a3dba92bc2da363987ebde5a68bd838e5b4d1
chensongxian <csx1164077611@gmail.com>
2021-02-25 11:09:37 +0800

    数据类型

---
+++ data_type/string_test.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+
+name = 'dawei'
+name_02 = '小马'
+
+print(id(name))
+print(id(name_02))
+
+new_name = name
+print(id(new_name))
+
+print(type(name))
+
+info = '''今天的天气真好呀""'''
+print(info)
+
+info1 = 'asdf'
+info2 = "asdf"
+
+new_str = "nihao dewei 'nihao' '' xiaomu"
+str_01 = ''
+str_02 = "aa" + "bb"
+print(len(str_01))
+print(new_str)
+print(str_02)
+print(len(str_02))
